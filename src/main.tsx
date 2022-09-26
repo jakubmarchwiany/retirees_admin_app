2499e923a7723a00d3a581760014c7340afdec06
xkvbikx <xkvbikx@gmail.com>
2022-09-26 21:09:38 +0200

    add LocalizationProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { ThemeProvider } from "@mui/material/styles";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { theme } from "assets/theme";
+import "dayjs/locale/pl";
 import { StrictMode } from "react";
 import App from "./App";
 import validateEnv from "./utils/validate-env";
@@ -11,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <StrictMode>
         <ThemeProvider theme={theme}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={"pl"}>
                             <App />
+            </LocalizationProvider>
         </ThemeProvider>
     </StrictMode>,
 );
