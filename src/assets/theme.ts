91557568d33a7060bb94ae3957eca585cf9f8f0f
xkvbikx <xkvbikx@gmail.com>
2022-09-26 21:08:24 +0200

    add theme (MUI)

---
+++ src/assets/theme.ts
@@ -0,0 +1,36 @@
+import "@fontsource/montserrat/300.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#133478",
+            contrastText: "#fff",
+        },
+        secondary: {
+            main: "#fff",
+            contrastText: "#133478",
+        },
+        background: {
+            default: "#fff",
+            paper: "#fff",
+        },
+        divider: "#D3D3D3",
+    },
+    typography: {
+        fontFamily: ["Montserrat", "sans-serif"].join(","),
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 769,
+            md: 1024,
+            lg: 1216,
+            xl: 1408,
+        },
+    },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "assets/theme";
 import { StrictMode } from "react";
 import App from "./App";
 import validateEnv from "./utils/validate-env";
@@ -8,6 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <StrictMode>
+        <ThemeProvider theme={theme}>
                             <App />
+        </ThemeProvider>
     </StrictMode>,
 );
