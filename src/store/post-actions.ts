a12db409e5bc8c6789d05f80f4991dcc3f612cc3
xkvbikx <xkvbikx@gmail.com>
2022-09-26 21:16:14 +0200

    create post actions

---
+++ src/store/post-actions.ts
@@ -0,0 +1,135 @@
+import { Dayjs } from "dayjs";
+import { NavigateFunction } from "react-router-dom";
+import { AppThunk } from "store";
+import { dataURLtoFile } from "utils/dataURLToFile";
+import { getFetch, imageFetch, postFetch } from "utils/fetches";
+import { appActions, PostType } from "./app-slice";
+import { uiActions } from "./ui-slice";
+
+export const getPosts =
+    (page: number): AppThunk =>
+    async (appDispatch) => {
+        await getFetch<{ posts: PostType[]; numberOfPages: number }>(
+            `/posts?page=${page}`,
+            appDispatch,
+            undefined,
+            undefined,
+            true,
+        )
+            .then(({ posts, numberOfPages }) => {
+                appDispatch(appActions.setPosts({ posts, numberOfPages }));
+                appDispatch(appActions.setLoading(false));
+            })
+            .catch(() => {
+                appDispatch(appActions.setPosts({ posts: [], numberOfPages: 0 }));
+                appDispatch(appActions.setLoading(false));
+            });
+    };
+
+export const newInformation =
+    (title: string, startDate: Dayjs, content: string, navigate: NavigateFunction): AppThunk =>
+    async (appDispatch) => {
+        if (title === "") {
+            appDispatch(uiActions.showErrorNotification("Musisz wprowadzić tytuł posta"));
+            return;
+        }
+        if (startDate === null) {
+            appDispatch(uiActions.showErrorNotification("Musisz wybrać datę rozpoczęcia podróży"));
+            return;
+        }
+        if (content === "") {
+            appDispatch(uiActions.showErrorNotification("Musisz wprowadzić treść informacji"));
+            return;
+        }
+
+        appDispatch(
+            uiActions.showNotification({
+                message: "Wszystkie dane poprawne. Wysyłam post ...",
+                type: "success",
+            }),
+        );
+
+        await postFetch<never>(
+            { title, startDate, content },
+            "/posts/new-information",
+            appDispatch,
+        ).then(() => {
+            navigate(`/admin/home`, { replace: true });
+            appDispatch(appActions.setLoading(true));
+        });
+    };
+
+export const newTrip =
+    (
+        title: string,
+        startDate: Dayjs,
+        endDate: Dayjs,
+        base64EncodedImage: string,
+        content: string,
+        navigate: NavigateFunction,
+    ): AppThunk =>
+    async (appDispatch) => {
+        if (title === "") {
+            appDispatch(uiActions.showErrorNotification("Musisz wprowadzić tytuł posta"));
+            return;
+        }
+        if (startDate === null) {
+            appDispatch(uiActions.showErrorNotification("Musisz wybrać datę rozpoczęcia podróży"));
+            return;
+        }
+
+        if (endDate === null) {
+            appDispatch(uiActions.showErrorNotification("Musisz wybrać datę zakończenia podróży"));
+            return;
+        }
+        if (base64EncodedImage === null) {
+            appDispatch(uiActions.showErrorNotification("Musisz dodać zdjęcie posta"));
+            return;
+        }
+        if (content === "") {
+            appDispatch(uiActions.showErrorNotification("Musisz wprowadzić treść posta"));
+            return;
+        }
+
+        appDispatch(
+            uiActions.showNotification({
+                message: "Wszystkie dane poprawne. Wysyłam post ...",
+                type: "success",
+            }),
+        );
+        const data = new FormData();
+
+        data.append("title", title);
+        data.append(
+            "startDate",
+            startDate.set("hour", 0).set("minute", 0).set("second", 0).toISOString(),
+        );
+        data.append(
+            "endDate",
+            endDate.set("hour", 0).set("minute", 0).set("second", 0).toISOString(),
+        );
+        data.append("image", dataURLtoFile(base64EncodedImage, "image.png"));
+
+        data.append("content", content);
+
+        await imageFetch<never>(data, "/posts/new-trip", appDispatch).then(() => {
+            navigate(`/admin/home`, { replace: true });
+            appDispatch(appActions.setLoading(true));
+        });
+    };
+
+export const deleteInformation =
+    (postID: string): AppThunk =>
+    async (appDispatch) => {
+        await getFetch<never>(`/posts/delete-information/${postID}`, appDispatch).then(() => {
+            appDispatch(appActions.setLoading(true));
+        });
+    };
+
+export const deleteTrip =
+    (postID: string): AppThunk =>
+    async (appDispatch) => {
+        await getFetch<never>(`/posts/delete-trip/${postID}`, appDispatch).then(() => {
+            appDispatch(appActions.setLoading(true));
+        });
+    };
