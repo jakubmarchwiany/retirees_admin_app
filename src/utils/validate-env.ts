c9bd84e696e77158103cd956dccc77f541ed4241
xkvbikx <xkvbikx@gmail.com>
2022-10-30 19:35:53 +0100

    change env variable name

--- src/utils/validate-env.ts
+++ src/utils/validate-env.ts
@@ -2,7 +2,7 @@ import { cleanEnv, str } from "envalid";
 
 function validateEnv() {
     cleanEnv(import.meta.env, {
-        VITE_DEV_BACKEND_URL: str(),
+        VITE_BACKEND_URL: str(),
 
         VITE_POSTS_FILE_NAME: str(),
         VITE_POSTS_FOLDER: str(),
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_DEV_BACKEND_URL: string;
+    readonly VITE_BACKEND_URL: string;
 
     readonly VITE_POSTS_FILE_NAME: string;
     readonly VITE_POSTS_FOLDER: string;
